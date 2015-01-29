47eb026459fed6feba8424389528c98096679224
Jacob Fritz <stjrf05@moravian.edu>
2015-01-29 15:29:04 -0500

    Fixed commit

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,26 @@ public class Main
   public static void main(String[] args)
   {
     // Create a new game with a cutoff of 18
-    PigGame g = new PigGame(18);
+    PigGame g = new PigGame(25);
     
     // Run one game
-    g.playGame();
+    //g.playGame();
     
+    //Find Turn Average
+    int Average = 0;
+    int games = 0;
+    while (games < 1000000)
+    {
+      g.playGame();
+      Average += g.getNumTurns();
+      games ++;
+    }
+    
+    Average /= 1000000;
+    System.out.println(Average);
     // output the results
-    System.out.println(g.getScore());
-    System.out.println(g.getNumTurns());
-    System.out.println(g.getTurnAverage());
+    //System.out.println(g.getScore());
+    //System.out.println(g.getNumTurns());
+    //System.out.println(g.getTurnAverage());
   }
 }
